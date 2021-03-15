831716644cb564e3fdbdd9c19e1dde9675a984cf
Leandro Guarino <le.guarino@gmail.com>
2021-03-15 00:41:09 -0300

    Corrigindo importacao nas novas telas

--- screens/Produtos.js
+++ screens/Produtos.js
@@ -1,3 +1,5 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
 export default function Produtos() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
