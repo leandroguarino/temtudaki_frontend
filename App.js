69b0d6b0a1518a63512f8106b83a4f79ccb3f8fc
Leandro Guarino <le.guarino@gmail.com>
2021-05-09 19:29:49 -0300

    Aula 36 - refresh de token

--- App.js
+++ App.js
@@ -10,6 +10,9 @@ import Principal from './screens/Principal';
 import Cadastro from './screens/Cadastro';
 import CadastroProduto from './screens/CadastroProduto';
 import CadastroServico from './screens/CadastroServico';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Config from './util/Config';
 
 const Stack = createStackNavigator();
 
@@ -25,7 +28,34 @@ function MyStack() {
   );
 }
 
+function defineInterceptor(){
+  axios.interceptors.response.use(response => {
+    return response
+  }, err => {
+    return new Promise((resolve, reject) => {
+      const originalReq = err.config
+      if (err.response.status == 401 && err.config && !err.config._retry){
+        originalReq._retry = true
+        AsyncStorage.getItem("TOKEN").then((token) => {
+          let res = axios.put(`${Config.API_URL}token/refresh`, {oldToken: token})
+          .then((res) => {
+            AsyncStorage.setItem("TOKEN", res.data.access_token)
+            originalReq.headers["Authorization"] = `Bearer ${res.data.access_token}`
+            return axios(originalReq)
+          })
+          resolve(res)
+        })
+      }else{
+        reject(err)
+      }
+    })
+  })
+}
+
 export default function App() {
+  
+  defineInterceptor()
+
   return (
     <NavigationContainer>
       <MyStack />
